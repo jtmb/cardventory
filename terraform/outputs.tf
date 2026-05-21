3d18f73da0c86966c1530e656cb5f19a24d18d12
jtmb <james.branco@gmail.com>
2026-05-21 09:35:48 -0400

    feat: bump version to 1.0.2 and update Terraform outputs to use tolist for IP address

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_ip" {
-  value       = linode_instance.cardventory.ipv4[0]
+  value       = tolist(linode_instance.cardventory.ipv4)[0]
   description = "Public IP address of the Cardventory Linode instance"
 }
 
 output "ssh_command" {
-  value       = "ssh root@${linode_instance.cardventory.ipv4[0]}"
+  value       = "ssh root@${tolist(linode_instance.cardventory.ipv4)[0]}"
   description = "SSH command to access the instance"
 }
